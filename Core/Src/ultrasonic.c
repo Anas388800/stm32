189421c2fc1aa724bff5405af91866299dfb5c93
Anas388800 <73471877+Anas388800@users.noreply.github.com>
2025-11-17 12:38:03 +0100

    augmentation vitesse du bip si distance <10cm

--- Core/Src/ultrasonic.c
+++ Core/Src/ultrasonic.c
@@ -83,10 +83,15 @@ void ultrasonic_task(void *argument)
             sprintf(msg, "Distance: %d cm\r\n", distance);
             UART_Debug(msg);
 
-            if (distance < 40) {
-                uint16_t buz = (uint16_t)map_int32(distance, 5, 40, 100, 900);
+            if (distance < 40 && distance > 10) {
+                uint16_t buz = (uint16_t)map_int32(distance, 5, 40, 100, 500);
                 buzzer_set_tone(buz);
-            } else {
+            }
+            else if (distance < 10 ){
+            	uint16_t buz = (uint16_t)map_int32(distance, 5, 40, 100, 200);
+            	buzzer_set_tone(buz);
+            }
+            else {
                 buzzer_set_tone(0);
             }
         }
